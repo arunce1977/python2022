23873dac87c4dc18f6b7d8ed6034b8e189e3fc23
arunce1977 <arun_ce74@yahoo.com>
2022-11-07 19:31:00 +0000

    mixed lists

---
+++ bestpractice.py
@@ -0,0 +1,25 @@
+#!/user/bin/env python3
+"""this is a best python practice class"""
+
+a=3.14
+
+def main():
+    my_string ="this is my string here we go..."
+    print(my_string)
+    print(a)
+    print("a = ", a)
+    print("a = \n", a)
+    print("a", "b")
+    print("a","b", sep="")
+    print(192,168,178,42,sep=".")
+    print("I am thinking", end="... ... ... \n")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
---
+++ list.py
@@ -0,0 +1,5 @@
+mylist = ['', 'testlist',"","test" ,'2','3', [1,2,3]]
+
+print("printing list "+ mylist[3])
+
+
